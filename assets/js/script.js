1a9008860f44207280e8db117df82f54c0effaa2
aracelly <aracellybootcamp@gmail.com>
2021-06-07 22:36:30 -0400

    se añade el nav

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+  /*
+   $(document).on("scroll", function(){
+    //sacamos el desplazamiento actual de la página
+    var desplazamientoActual = $(document).scrollTop();
+
+    if(desplazamientoActual > 100){
+         $('nav').addClass('my-bg-nav');
+    }
+    else {
+       $('nav').removeClass('my-bg-nav');
+     }
+
+});
+
+*/
+
+
+
+
+});
